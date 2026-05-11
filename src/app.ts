f5e24616f902d81988e5a37b69a2ff0334428167
AlenChen0216 <64183640+AlenChen0216@users.noreply.github.com>
2026-05-11 23:27:58 +0800

    error test 3

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 import Fastify, { FastifyServerOptions } from 'fastify';
 
-export function buildApp(options: FastifyServerOptions = {}) {
+export function buildApp(options: FastifyServerOptions={}) {
   const APP_1 = Fastify({
     logger: options.logger ?? true,
     ...options
